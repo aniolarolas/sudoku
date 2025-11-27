79c62afedff8e0e9a1e6bda5361bb85f7ae2dcdd
aniolarolas <aniol.arolas@estudiantat.upc.edu>
2025-11-27 15:42:16 +0100

    Update casilla.h

--- casilla.h
+++ casilla.h
@@ -1,1 +1,28 @@
+/* 
+**  Fitxer:      casilla.h
+**  Autor:       (Equip)
+**  Data:        (AAAA-MM-DD)
+**  Descripció:  Definició de t_casilla i prototips d’operacions sobre casella.
+*/
+#ifndef CASILLA_H
+#define CASILLA_H
+
+/* Fichers de capçalera del sistema (si calgués) */
+/* #include <stdio.h> */
+
+/* Constants simbòliques (si calgués) */
+/* #define ... */
+
+/* Tipus de dades */
+typedef struct {
+    char car;        /* ' ', '0'..'9', 'a'..'z', 'A'..'Z' */
+    int  modificable;/* 0 no, 1 si */
+} t_casilla;
+
+/* Prototips de funcions (accions sobre casella) */
+void inicialitzar_casilla(char car, t_casilla *c);
+void imprimir_casilla(t_casilla c);
+void canviar_casilla(t_casilla *c, char car);
+
+#endif /* CASILLA_H */
 
