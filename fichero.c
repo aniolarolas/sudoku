6e767ba2b30e456b29e1576ad99ac749b6416019
Drofius13 <bhcd8ctycw@privaterelay.appleid.com>
2025-12-18 15:13:09 +0100

    Update fichero.c

--- fichero.c
+++ fichero.c
@@ -52,3 +52,64 @@ void cerrar_fichero() {
 	fclose(set_or_get_fichero(NULL));
 }
 
+
+// CORREGIDO (2 errores en char y en feof)
+
+#include <stdio.h>
+#include "fichero.h"
+
+FILE *set_or_get_fichero(FILE *f) {
+    static FILE *fichero = NULL;
+
+    if (f != NULL) {
+        fichero = f;
+    }
+    return fichero;
+}
+
+int abrir_fichero(char nombre_fichero[]) {
+    set_or_get_fichero(fopen(nombre_fichero, "r"));
+    if (set_or_get_fichero(NULL) == NULL) {
+        return ABRIR_FICHERO_ERR;
+    }
+    return ABRIR_FICHERO_OK;
+}
+
+int leer_int_fichero() {
+    int num;
+    FILE *f = set_or_get_fichero(NULL);
+
+    if (f == NULL) return -1;
+
+    if (fscanf(f, "%d", &num) == EOF) {
+        return -1;
+    }
+    return num;
+}
+
+int leer_char_fichero() {   // <- cambiado a int
+    int car;
+    FILE *f = set_or_get_fichero(NULL);
+
+    if (f == NULL) return EOF;
+
+    car = fgetc(f);
+    return car;   // devuelve char o EOF
+}
+
+int es_fin_de_fichero() {
+    FILE *f = set_or_get_fichero(NULL);
+    if (f == NULL) return 1;
+    return feof(f);
+}
+
+void cerrar_fichero() {
+    FILE *f = set_or_get_fichero(NULL);
+    if (f != NULL) {
+        fclose(f);
+        set_or_get_fichero(NULL);
+    }
+}
+
+
+
