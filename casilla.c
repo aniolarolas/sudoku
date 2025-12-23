332118864fd575607d0949b995039165d51bc148
Drofius13 <bhcd8ctycw@privaterelay.appleid.com>
2025-12-23 22:32:35 +0100

    Update casilla.c

--- casilla.c
+++ casilla.c
@@ -26,3 +26,51 @@ void canviar_casilla(t_casilla *c, char car)
     /* TODO: if (c->modificable) { c->car = car; } */
 }
 
+
+
+//CASILLA.C
+
+#include "casilla.h"
+#include <stdio.h>
+
+/*
+ * Inicialitza una casella.
+ * Si el caràcter és espai (' '), la casella és modificable.
+ * En cas contrari, és fixa.
+ */
+void inicialitzar_casilla(char car, t_casilla *c)
+{
+    if (c == NULL) {
+        return;
+    }
+
+    c->car = car;
+
+    if (car == ' ') {
+        c->modificable = 1;
+    } else {
+        c->modificable = 0;
+    }
+}
+
+/*
+ * Imprimeix el contingut de la casella.
+ */
+void imprimir_casilla(t_casilla c)
+{
+    printf("%c", c.car);
+}
+
+/*
+ * Canvia el valor de la casella només si és modificable.
+ */
+void canviar_casilla(t_casilla *c, char car)
+{
+    if (c == NULL) {
+        return;
+    }
+
+    if (c->modificable == 1) {
+        c->car = car;
+    }
+}
