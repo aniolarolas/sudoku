b81a5955531c279eb301df68001f6fea04353f9e
aniolarolas <aniol.arolas@estudiantat.upc.edu>
2025-12-24 13:41:15 +0100

    Revise main function for Sudoku file handling
    Refactor main function to handle file input and Sudoku initialization.

--- sudokubo/casilla.c
+++ sudokubo/casilla.c
@@ -1,36 +1,48 @@
+/* 
+**  Fitxer:      main.c
+**  Data:        27/11/2025
+**  Descripció:  Punt d’entrada del Sudoku. Orquestra lectura, inicialització,
+**               bucle de jugades i comprovació de final segons rúbrica.
+*/
+
 #include <stdio.h>
-#include "casilla.h"
-#include "colores.h"
+#include "fichero.h"
+#include "tablero.h"
 
-int es_casella_buida(char c)
-{
-    return (c == '.') || (c == '-') || (c == ' ');
-}
+#define SIZE_NOMBRE_FICHERO 64
 
-void inicialitzar_casilla(char car, t_casilla *c)
+int main(void)
 {
-    c->car = car;
-    if (es_casella_buida(car)) c->modificable = 1;
-    else c->modificable = 0;
-}
+    char nom_fichero[SIZE_NOMBRE_FICHERO];
+    t_tablero t;
 
-void imprimir_casilla(t_casilla c)
-{
-    if (c.modificable == 0) {
-        printf_color_negrita();
-        if (es_casella_buida(c.car)) printf(" ");
-        else printf("%c", c.car);
-        printf_reset_color();
-    } else {
-        if (es_casella_buida(c.car)) printf(" ");
-        else printf("%c", c.car);
+    printf("Intro sudoku file name: ");
+    scanf("%63s%*c", nom_fichero);
+
+    /* 1) Obrir fitxer amb la llibreria donada */
+    if (abrir_fichero(nom_fichero) != ABRIR_FICHERO_OK) {
+        printf("Error: El fichero no existe.\n");
+        return 1;
     }
-}
 
-void canviar_casilla(t_casilla *c, char car)
-{
-    if (c->modificable != 0) {
-        c->car = car;
+    /* 2) Inicialitzar tauler des del fitxer */
+    if (inicialitzar_tablero(&t) != 0) {
+        printf("Error: formato de fichero no valido.\n");
+        cerrar_fichero();
+        return 1;
     }
-}
 
+    /* 3) Mostrar tauler inicial */
+    imprimir_tablero(t);
+
+    /* 4) Bucle de joc: jugades fins que estigui finalitzat */
+    while (sudoku_finalitzat(t) == 0) {
+        realitzar_jugada(&t);
+        imprimir_tablero(t);
+    }
+
+    /* 5) Final */
+    printf("FELICIDADES: Sudoku resuelto! :-)\n");
+    cerrar_fichero();
+    return 0;
+}
