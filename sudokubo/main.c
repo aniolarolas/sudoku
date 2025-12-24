2f0596f45d7c9c11a4f62dc42dab77ea3467e338
aniolarolas <aniol.arolas@estudiantat.upc.edu>
2025-12-24 13:44:27 +0100

    canvis main

--- sudokubo/main.c
+++ sudokubo/main.c
@@ -1,38 +1,47 @@
-/*
+/* 
 **  Fitxer:      main.c
-**  Descripció:  Punt d’entrada del Sudoku.
+**  Data:        27/11/2025
+**  Descripció:  Punt d’entrada del Sudoku. Orquestra lectura, inicialització,
+**               bucle de jugades i comprovació de final segons rúbrica.
 */
+
 #include <stdio.h>
 #include "fichero.h"
 #include "tablero.h"
 
+#define SIZE_NOMBRE_FICHERO 64
 
-int main()
+int main(void)
 {
     char nom_fichero[SIZE_NOMBRE_FICHERO];
     t_tablero t;
 
     printf("Intro sudoku file name: ");
-    scanf("%79s%*c", nom_fichero);
+    scanf("%63s%*c", nom_fichero);
 
+    /* 1) Obrir fitxer amb la llibreria donada */
     if (abrir_fichero(nom_fichero) != ABRIR_FICHERO_OK) {
         printf("Error: El fichero no existe.\n");
         return 1;
     }
 
+    /* 2) Inicialitzar tauler des del fitxer */
     if (inicialitzar_tablero(&t) != 0) {
         printf("Error: formato de fichero no valido.\n");
         cerrar_fichero();
         return 1;
     }
 
+    /* 3) Mostrar tauler inicial */
     imprimir_tablero(t);
 
+    /* 4) Bucle de joc: jugades fins que estigui finalitzat */
     while (sudoku_finalitzat(t) == 0) {
         realitzar_jugada(&t);
         imprimir_tablero(t);
     }
 
+    /* 5) Final */
     printf("FELICIDADES: Sudoku resuelto! :-)\n");
     cerrar_fichero();
     return 0;
