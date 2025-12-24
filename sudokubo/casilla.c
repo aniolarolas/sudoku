9cd3681221e7d56a959ec19a8e87c27c0328560f
aniolarolas <aniol.arolas@estudiantat.upc.edu>
2025-12-24 13:49:57 +0100

    Add function to convert lowercase to uppercase

--- sudokubo/casilla.c
+++ sudokubo/casilla.c
@@ -2,6 +2,15 @@
 #include "casilla.h"
 #include "colores.h"
 
+/* Convertir lletra minúscula a majúscula */
+char a_majuscula(char c)
+{
+    if (c >= 'a' && c <= 'z') {
+        return c - ('a' - 'A');
+    }
+    return c;
+}
+
 int es_casella_buida(char c)
 {
     return (c == '.') || (c == '-') || (c == ' ');
@@ -9,6 +18,7 @@ int es_casella_buida(char c)
 
 void inicialitzar_casilla(char car, t_casilla *c)
 {
+    car = a_majuscula(car);
     c->car = car;
     if (es_casella_buida(car)) c->modificable = 1;
     else c->modificable = 0;
@@ -30,6 +40,6 @@ void imprimir_casilla(t_casilla c)
 void canviar_casilla(t_casilla *c, char car)
 {
     if (c->modificable != 0) {
-        c->car = car;
+        c->car = a_majuscula(car);
     }
 }
